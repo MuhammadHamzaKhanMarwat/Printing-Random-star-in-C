21e8cb0bcec6dc3255ab8a7339512282c2c7d862
MuhammadHamzaKhanMarwat <141237221+MuhammadHamzaKhanMarwat@users.noreply.github.com>
2023-08-13 09:51:35 +0500

    Add files via upload

---
+++ 22P-9178_Hamza_Khan__PF_Theory_Assignment_2.c
@@ -0,0 +1,56 @@
+#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
+#include<unistd.h> 
+#include<windows.h> // for Gotoxy cursor function.
+void gotoxy(int x ,int y)
+{
+    COORD cordination = {x,y};
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),cordination);
+}
+int main()
+{
+    int i , j ;
+    int rows = 10 , colonm = 70;
+    system("color 0"); //Background color black
+    srand(time(NULL)); //Random Number Generator
+    while(1)  
+    {
+        for ( i = 0; i < rows; i++)
+        {
+            for (j = 0; j < colonm; j++)
+            {
+                if (rand() % 10 == 0)
+                {
+                    printf(" * ");
+                }
+                else
+                {
+                    printf(" ");
+                }            
+            }
+            printf("\n");
+        }
+        gotoxy(35,10); // Positioning the text in the middle use Gotoxy
+		printf("WELCOME TO THE GAME SHOW\n");
+        for ( i = 0; i < rows; i++)
+        {
+            for (j = 0; j < colonm; j++)
+            {
+                if (rand() % 10 == 0)
+                {
+                    printf(" * ");
+                }
+                else
+                {
+                    printf(" ");
+                }            
+            }
+            printf("\n");
+        }
+         sleep(1); // Freezes the program for 1 sec before before printing another set of stars
+		 system("cls"); // Removing the previous set of stars  
+
+    } 
+
+}
